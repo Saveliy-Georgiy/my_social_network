da9e284e8a10dc2ca27cda9b4faa6df517980ee8
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2020-02-15 14:50:08 +0300

    Updates

---
+++ precache-manifest.d9606a4af7d9b817d670d77dcf59e455.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51237e18d7151126bdf30aea5fce22d3",
+    "url": "/my_social_network/index.html"
+  },
+  {
+    "revision": "4e102be21185f69d8bd3",
+    "url": "/my_social_network/static/css/main.f6360d0b.chunk.css"
+  },
+  {
+    "revision": "713c1efd0effe5a73626",
+    "url": "/my_social_network/static/js/2.2322f1bf.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/my_social_network/static/js/2.2322f1bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e102be21185f69d8bd3",
+    "url": "/my_social_network/static/js/main.d80d087c.chunk.js"
+  },
+  {
+    "revision": "ee14f96732aed2937863",
+    "url": "/my_social_network/static/js/runtime-main.f2896c50.js"
+  },
+  {
+    "revision": "1a2864695639422914e098841bf41e0c",
+    "url": "/my_social_network/static/media/avatar1.1a286469.jpg"
+  },
+  {
+    "revision": "dfac1092924a358543275ba2ad4938a1",
+    "url": "/my_social_network/static/media/background.dfac1092.jpg"
+  },
+  {
+    "revision": "7901018ae3a8ce847a72b8f5207e0f18",
+    "url": "/my_social_network/static/media/logo1.7901018a.png"
+  }
+]);
